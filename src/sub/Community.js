7d9d15562612f4894fadd9a7b91f583fca2b0a9b
uvin1016 <uvin1016@naver.com>
2021-12-24 21:57:08 +0900

    axios 설정 후 게시판 데이터 작성, community.js에 동적으로 게시글 생성(역순으로)

--- src/sub/Community.js
+++ src/sub/Community.js
@@ -1,8 +1,39 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function Community(){
+    let [posts,setPosts] = useState([]);
+    let len = posts.length;
+
+    useEffect(()=>{
+        axios
+        .get('./dbs/board.json')
+        .then((data)=>{
+            setPosts(data.data.data);
+        });
+    });
+
     return (
         <section className="content community">
             <div className="inner">
                 <h1>Community</h1>
+                {
+                    posts.slice(0).reverse().map((post,index)=>{
+                        return (
+                            <article key={index}>
+                                <p className="num">{len - index}</p>
+                                <div className="wrap">
+                                    <h2>{post.title}</h2>
+                                    <div className="txtWrap">
+                                        <span>{post.writer}</span>
+                                        <span>{post.date}</span>
+                                        <span>{post.like}</span>
+                                    </div>
+                                </div>
+                            </article>
+                        )
+                    })
+                }
             </div>
         </section>
     )
