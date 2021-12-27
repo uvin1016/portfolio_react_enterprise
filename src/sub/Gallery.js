d642156c39b916f30d56db9ee4c5871bd4521941
uvin1016 <uvin1016@naver.com>
2021-12-27 22:23:57 +0900

    galley.html method 변경

--- src/sub/Gallery.js
+++ src/sub/Gallery.js
@@ -4,40 +4,41 @@ import { useEffect, useRef, useState } from "react";
 function Gallery(){
     const baseURL = "https://www.flickr.com/services/rest/?";
     const key = "04c5ace5347f338643f5a46006aa1910";
+    const userId = "194310676@N02";
     const method1 = "flickr.interestingness.getList";
+    const method2 = "flickr.people.getPhotos";
     const count = 12;
 
-    const url = `${baseURL}method=${method1}&api_key=${key}&per_page=${count}&format=json&nojsoncallback=1`;
+    const url = `${baseURL}method=${method2}&api_key=${key}&user_id=${userId}&per_page=${count}&format=json&nojsoncallback=1`;
 
     let [imgs,setImgs] = useState([]);
     let frame = useRef(null);
 
-    useEffect(getFlickr,[]);
+    useEffect(()=>{
+        getFlickr();
+    },[]);
 
     async function getFlickr(){
-        await axios
-        .get(url)
-        .then(json=>{
+        await axios.get(url).then(json=>{
             setImgs(json.data.photos.photo);
-            console.log(json.data.photos.photo);
-        });
+        },1000);
 
-        setTimeout(()=>{
-            frame.current.classList.add("on");
-        },500)
-    };
+        frame.current.classList.add("on");
+    }
+
 
     return (
         <section className="content gallery">
             <div className="inner">
                 <h1>Gallery</h1>
+
                 <div className="imgWrap" ref={frame}>
                     {
                         imgs.map((img,index)=>{
-                            const imgSrc = `https://live.staticflickr.com/${img.server}/${img.id}_${img.secret}_m.jpg`;
-                            const imgSrcBig = `https://live.staticflickr.com/${img.server}/${img.id}_${img.secret}_b.jpg`;
+                            let imgSrcBig = `https://live.staticflickr.com/${img.server}/${img.id}_${img.secret}_b.jpg`;
+                            let imgSrc = `https://live.staticflickr.com/${img.server}/${img.id}_${img.secret}_m.jpg`;
 
-                            return (
+                            return(
                                 <article key={index}>
                                     <a href={imgSrcBig}>
                                         <img src={imgSrc} />
