c1884b3cbf08682133fca77e6fb0410e9ba877ee
uvin1016 <uvin1016@naver.com>
2021-12-28 12:12:37 +0900

    Masonry를 이용하여 이미지 정렬시킴, button을 추가하여 해당 이름에 맞는 이미지 불러오기

--- src/sub/Gallery.js
+++ src/sub/Gallery.js
@@ -1,5 +1,12 @@
 import axios from "axios";
 import { useEffect, useRef, useState } from "react";
+import Masonry from "react-masonry-component";
+
+const masonryOptions = {
+    fitWidth: false,
+    gutter: 0,
+    itemSelector: ".items"
+}
 
 function Gallery(){
     const baseURL = "https://www.flickr.com/services/rest/?";
@@ -7,24 +14,21 @@ function Gallery(){
     const userId = "194310676@N02";
     const method1 = "flickr.interestingness.getList";
     const method2 = "flickr.people.getPhotos";
+    const method3 = "flickr.favorites.getList";
     const count = 12;
+    
 
-    const url = `${baseURL}method=${method2}&api_key=${key}&user_id=${userId}&per_page=${count}&format=json&nojsoncallback=1`;
+    const url = `${baseURL}method=${method1}&api_key=${key}&per_page=${count}&format=json&nojsoncallback=1`;
+    const url2 = `${baseURL}method=${method2}&api_key=${key}&user_id=${userId}&per_page=${count}&format=json&nojsoncallback=1`;
+    const url3 = `${baseURL}method=${method3}&api_key=${key}&user_id=${userId}&per_page=${count}&format=json&nojsoncallback=1`;
 
     let [imgs,setImgs] = useState([]);
-    let frame = useRef(null);
+    let list = useRef(null);
 
     useEffect(()=>{
-        getFlickr();
+        getFlickr(url);
     },[]);
 
-    async function getFlickr(){
-        await axios.get(url).then(json=>{
-            setImgs(json.data.photos.photo);
-        },1000);
-
-        frame.current.classList.add("on");
-    }
 
 
     return (
@@ -32,30 +36,75 @@ function Gallery(){
             <div className="inner">
                 <h1>Gallery</h1>
 
-                <div className="imgWrap" ref={frame}>
-                    {
-                        imgs.map((img,index)=>{
-                            let imgSrcBig = `https://live.staticflickr.com/${img.server}/${img.id}_${img.secret}_b.jpg`;
-                            let imgSrc = `https://live.staticflickr.com/${img.server}/${img.id}_${img.secret}_m.jpg`;
+                <div className="btns">
+                    <button className="on" onClick={(e)=>{
+                        list.current.classList.remove("on");
+                        btnActive(e.target);
+                        getFlickr(url);
+                    }}>Interest</button>
 
-                            return(
-                                <article key={index}>
-                                    <a href={imgSrcBig}>
-                                        <img src={imgSrc} />
+                    <button onClick={(e)=>{
+                        list.current.classList.remove("on");
+                        btnActive(e.target);
+                        getFlickr(url2);
+                    }}>Architecture</button>
 
-                                        <div className="txtBox">
-                                            <h2>{img.title}</h2>
-                                            <p>{img.owner}</p>
-                                        </div>
-                                    </a>
-                                </article>
-                            )
-                        })
-                    }
+                    <button onClick={(e)=>{
+                        list.current.classList.remove("on");
+                        btnActive(e.target);
+                        getFlickr(url3);
+                    }}>Landscape</button>
+                </div>
+
+                <div className="imgWrap" ref={list}>
+                    <Masonry
+                        className={"frame"}
+                        elementType={"ul"}
+                        disableImagesLoaded={false}
+                        updateOnEachImageLoad={false}
+                        options={masonryOptions}
+                    >
+                        {
+                            imgs.map((img,index)=>{
+                                let imgSrc = `https://live.staticflickr.com/${img.server}/${img.id}_${img.secret}_b.jpg`;
+
+                                return(
+                                    <li key={index} className="items">
+                                        <a href={imgSrc} >
+                                            <img src={imgSrc} />
+
+                                            <div className="txtBox">
+                                                <h2>{img.title}</h2>
+                                                <p>{img.owner}</p>
+                                            </div>
+                                        </a>
+                                    </li>
+                                )
+                            })
+                        }
+                    </Masonry>
                 </div>
             </div>
         </section>
     )
+
+    async function getFlickr(url){
+        await axios.get(url).then(json=>{
+            setImgs(json.data.photos.photo);
+        },1000);
+
+        setTimeout(()=>{
+            list.current.classList.add("on");
+        },500);
+    }
+
+    function btnActive(btn){
+        const galleryBtn = document.querySelectorAll(".gallery .inner .btns button");
+        for(let btn of galleryBtn){
+            btn.classList.remove("on");
+        }
+        btn.classList.add("on");
+    }
 }
 
 export default Gallery;
