3f6396f3185018ebd6769a3d86dd77354a57b88c
uvin1016 <uvin1016@naver.com>
2022-01-07 23:16:11 +0900

    main 페이지 스타일 작성/ 윈도우 스크롤시 figure이동시키기

--- src/main/main.js
+++ src/main/main.js
@@ -1,9 +1,39 @@
+import { useEffect } from "react";
+import { useRef } from "react/cjs/react.development";
+
 function Main(){
+    const baseURL = process.env.PUBLIC_URL;
+    const frame = useRef(null);
+
+    useEffect(()=>{
+        window.addEventListener("scroll",()=>{
+            vidMove();
+        })
+
+        return ()=>{
+            window.removeEventListener("scroll",()=>{
+                vidMove();
+            })
+        }
+    },[]);
+
     return (
         <section className="content main">
-            <div className="inner"></div>
+            <div className="inner">
+                <div className="visual">
+                    <h1>Investment<br />Group <span>CHRISTY<br />MONRAY</span></h1>
+                    <figure ref={frame}>
+                        <video src={`${baseURL}/vid/Waves.mp4`} autoPlay muted loop></video>
+                    </figure>
+                </div>
+            </div>
         </section>
     )
+
+    function vidMove (){
+        const scrollValue = window.scrollY || window.pageYOffset;
+        if(scrollValue > 0) frame.current.classList.add("on");
+    }
 }
 
 export default Main;
